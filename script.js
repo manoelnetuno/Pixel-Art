f1af6ba32500484c0cefd24ebe9f52cdb5b55ce7
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2023-07-17 09:29:35 -0300

    requisito-1

---
+++ script.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1 id="title">Paleta de Cores</h1>
+    <div id="color-palette">
+        <div class="color-box color" style="background-color: #87CEEB;"></div>
+        <div class="color-box color" style="background-color: #32CD32;"></div>
+        <div class="color-box color" style="background-color: #FF1493;"></div>
+        <div class="color-box color" style="background-color: #FFA500;"></div>
+    </div>
+</body>
+<script src="script.js"></script>
+</body>
+
+</html>
