88ac8989cc2a7a27d0eaaa00ae21a8524299b2f2
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2023-07-19 15:59:56 -0300

    função-para-selecionar-cor

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ for (let i = 0; i < 25; i++) {
   pixel.classList.add('pixel');
   pixelBoard.appendChild(pixel);
 }
+function selectColor(colorElement) {
+  const colorElements = document.querySelectorAll('.color');
+
+  colorElements.forEach((element) => {
+    if (element === colorElement) {
+      element.classList.add('selected');
+    } else {
+      element.classList.remove('selected');
+    }
+  });
+}
